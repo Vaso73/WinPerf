5fde400c83c0fb61314854763de39cd22ab7515c
VAIO <info@watel.sk>
2026-04-27 02:40:08 +0000

    Initialize WinPerf solution skeleton

---
+++ src/WinPerf.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace WinPerf.Core;
+
+public class Class1
+{
+
+}
---
+++ tests/WinPerf.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WinPerf.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
